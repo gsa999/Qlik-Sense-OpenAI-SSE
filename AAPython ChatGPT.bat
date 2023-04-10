667abcfb21c03b3224a9d543de02ef633ab15d83
Guru Arora <hkp@qlik.com>
2023-04-10 21:19:31 +0530

    first commit

---
+++ AAPython ChatGPT.bat
@@ -0,0 +1,5 @@
+@echo off
+set PATH=C:\Users\hkp\AppData\Local\Programs\Python\Python36\;%PATH%
+set PYTHONPATH=C:\Users\hkp\OneDrive - QlikTech Inc\Guru\@Products\chatgpt\ChatGPT
+cd "C:\Users\hkp\OneDrive - QlikTech Inc\Guru\@Products\chatgpt\ChatGPT"
+python __main__.py
